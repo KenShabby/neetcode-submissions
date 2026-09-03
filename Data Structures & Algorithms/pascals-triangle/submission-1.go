a92747e578772b5a06bde92e74df91cf34f89da9
Brendan Coen <kshabby@sonic.net>
2026-09-02 20:14:26 -0700

    Add: pascals-triangle - submission-1

---
+++ Data Structures & Algorithms/pascals-triangle/submission-1.go
@@ -0,0 +1,13 @@
+func generate(numRows int) [][]int {
+	var res = make([][]int, numRows)
+
+	for i := 0; i < numRows; i++ {
+		row := make([]int, i+1)
+		row[0], row[i] = 1, 1
+		for j := 1; j < i; j++ {
+			row[j] = res[i-1][j-1] + res[i-1][j]
+		}
+		res[i] = row
+	}
+	return res
+}
